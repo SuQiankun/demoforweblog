4edf26ecbf54448c37406fb6e8dfe19cd6da42d5
SuQiankun <771909584@qq.com>
2017-05-13 23:55:12 +0800

    TODO: 使用 tp 框架实现登录校验

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -1,3 +1,18 @@
 <?php
+namespace Home\Controller;
+use Think\Controller;
 
-?>
+class LoginController extends Controller{
+
+    public function loginAction($username,$password)
+    {
+        $Use = M('User');
+        $result = $Use->find($username);
+//        $this->assign();
+        $this->display();
+
+    }
+
+
+
+}
