1d29e4f9522663cea95739c46f3b8619d2a2fc8b
SuQiankun <771909584@qq.com>
2017-05-13 11:29:26 +0800

    测试模板文件,控制器

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Common 公共函数
 Conf 核心配置文件
 Language 语言
+Library 核心类库
--- index.php
+++ index.php
@@ -19,7 +19,8 @@ define('APP_DEBUG',True);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
-
+//快速生成 admin 模块
+//define('BIND_MODULE','Admin');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
