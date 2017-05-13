4d80c77f25c002664f8baa59b4ed175677937992
SuQiankun <771909584@qq.com>
2017-05-13 17:19:44 +0800

    1. 读取数据库数据

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -4,9 +4,7 @@ use Think\Controller;
 class FormController extends Controller{
     //插入数据的操作
     public function insert(){
-
         //使用 D 函数,需要有model 类
-
         $Form   =   D('Form');
         if($Form->create()) {
             $result =   $Form->add();
@@ -19,5 +17,20 @@ class FormController extends Controller{
             $this->error($Form->getError());
         }
     }
+
+    public function read($id=0){
+        $Form   =   M('Form');
+        // 读取数据
+        $data =   $Form->find($id);
+        $this->assign('data',$data);// 模板变量赋值
+//        if($data) {
+//            $this->assign('data',$data);// 模板变量赋值
+//        }else{
+//            $this->error('数据错误');
+//        }
+        $this->display();
+    }
+
+
 
  }
