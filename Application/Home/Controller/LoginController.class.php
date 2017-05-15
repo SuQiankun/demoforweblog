7ae0ab8e86e1fa0661b3cb33beb3686ae4e5f404
SuQiankun <771909584@qq.com>
2017-05-15 12:49:46 +0800

    登陆功能

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -4,12 +4,25 @@ use Think\Controller;
 
 class LoginController extends Controller{
 
-    public function loginAction($username,$password)
+    public function loginIn($username,$password)
     {
+        echo($username);
+        echo($password);
+
         $Use = M('User');
-        $result = $Use->find($username);
-//        $this->assign();
-        $this->display();
+        $params['username']=$username;
+
+        $result = $Use->where($params)->select();
+
+        if($result[0]['username']){
+
+            if($result[0]['password'] == $password){
+                echo ('登陆成功');
+            }else{
+                echo ('登陆失败');
+            }
+
+        }
 
     }
 
