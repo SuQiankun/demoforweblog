3478395a636390c7e636adb8be2ee7a19b2c1ddf
SuQiankun <771909584@qq.com>
2017-06-12 23:15:03 +0800

    1. 修改一条数据到数据库 2. 使用 find 从数据库中获取一维数组的信息 3. $this ->redirect(‘action’, array(params), time, ‘msg’); 跳转方法的使用

--- Application/Home/Controller/NodePadController.class.php
+++ Application/Home/Controller/NodePadController.class.php
@@ -7,8 +7,12 @@ class NodePadController extends Controller{
 
     public function showTipList(){
 		$WORD = M('word');
-		$map['sgNumber'] = array('gt',5);
-		$result = $WORD->where($map)->select();
+//		$map['sgNumber'] = array('gt',5);
+//		$result = $WORD->where($map)->select();
+
+//		$result= $WORD->limit('0,10')->select();
+
+		$result = $WORD -> limit(5)->page(3)->select();
 		foreach($result as $k=>&$v){
 			$v['index'] = $v['sgnumber'];
 			 unset($v['sgnumber']);
@@ -22,6 +26,26 @@ class NodePadController extends Controller{
 //		$this -> ajaxReturn($resultData);
     }
 
+    
+	function updateInfo($infoid){
+		$info = D('word');
+
+        //更新数据, else 赋值
+        if(!empty($_POST)){
+            $data = $info ->find($infoid);
+            $info->save($data);
+        }else{
+
+           $word = $info ->find($infoid);
+            $this-> assign('info',$word);
+            $this->display();
+        }
+
+
+
+
+
+	}
 
 
 }
