0bd0d30d0d81ff53a12a0f2f8c66d0884605cd27
SuQiankun <771909584@qq.com>
2017-06-21 15:38:00 +0800

    1. 添加数据分页 LoginController -> index  —> index.html

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -1,9 +1,39 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Think\Page;
 use Think\Verify;
 
 class LoginController extends Controller{
+
+    function index()
+    {
+        $data  = M('userinfo');
+        $map['user_id'] = array('gt',0);
+        $count = $data->where($map)->count();
+
+
+        $page = new Page($count,'1');
+
+        $page->setConfig('header',' 条记录 ');
+        $page->setConfig('prev',' 上一页 ');
+        $page->setConfig('next',' 下一页 ');
+        $page->setConfig('first',' 首页 ');
+        $page->setConfig('last',' 尾页 ');
+
+        $show = $page->show();
+
+        //$list = $Data->where($map)->order('create_time')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $list = $data->where($map)->order('user_id')->limit($page->firstRow.','.$page->listRows)->select();
+
+        $this->assign('list',$list);
+        $this->assign('page',$show);
+
+        $this->display();
+
+
+    }
+
     public function login(){
 
         if (!empty($_POST)){
@@ -18,7 +48,14 @@ class LoginController extends Controller{
                 $info = D('userinfo')->where($userpsw)->find();
 
                 if ($info){
-                    echo '登录失败';
+                    echo '登陆成功';
+                    $ip = get_client_ip();
+                    echo $ip;
+
+                    $ad = new \Org\Net\IpLocation('UTFry.dat');
+                    echo $ad->getlocation($ip);
+
+
                 }else{
                     echo '登录失败';
                 }
@@ -46,6 +83,7 @@ class LoginController extends Controller{
         $vry = new \Think\ Verify($cfg);
         $vry->entry();
     }
+
 
 
 }
