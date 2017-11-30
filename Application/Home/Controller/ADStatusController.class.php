d06d0e0707105eb5b15097042557c4f821bcff7c
SuQiankun <771909584@qq.com>
2017-11-30 16:16:29 +0800

    change state on

--- Application/Home/Controller/ADStatusController.class.php
+++ Application/Home/Controller/ADStatusController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class ADStatusController extends Controller{
     public function index(){
-        $str['stauts'] = 'off';
+        $str['stauts'] = 'on';
         $jsonStr = json_encode($str);
         exit($jsonStr);
     }   
